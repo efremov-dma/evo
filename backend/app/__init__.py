72a95385e1ed3730d01a8dc87052d42207a0452e
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-25 11:48:12 +0000

    Integrated sql alchemy and migrations.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_object('app.settings.base')
+
+db = SQLAlchemy()
+db.init_app(app)
+Migrate(app, db)
 
 from . import departments
