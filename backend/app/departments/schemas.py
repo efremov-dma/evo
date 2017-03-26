1b9c360578695209c33aa5d87546049c191bf802
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-26 11:21:05 +0000

    Added department head validation.

--- backend/app/departments/schemas.py
+++ backend/app/departments/schemas.py
@@ -1,5 +1,12 @@
+from flask import request
+from marshmallow import ValidationError
 from marshmallow import fields, validate
+from marshmallow import post_load
+from marshmallow import validates
 
+from app.employees.models import Employee
+from app.errors.errors import InvalidAttribute
+from app.errors.exceptions import NotFound, BadRequest
 from app.schemas import ModelSchema
 
 
@@ -7,3 +14,22 @@ class DepartmentSchema(ModelSchema):
     name = fields.Str(required=True, validate=[validate.Length(max=255)])
     description = fields.Str(allow_none=True)
     head_id = fields.UUID(allow_none=True)
+
+    @validates('head_id')
+    def validate_head_id(self, head_id):
+        if not head_id:
+            return True
+        try:
+            employee = Employee.get_or_404(head_id.hex)
+        except NotFound:
+            raise ValidationError('Employee does not exist.')
+
+        if not employee.works_in_department(request.view_args.get('id')):
+            raise ValidationError('Employee does not work in this department.')
+
+    @post_load
+    def uuid_to_hex(self, item):
+        uuid = item.get('head_id')
+        if uuid:
+            item['head_id'] = uuid.hex
+        return item
--- backend/app/employees/models.py
+++ backend/app/employees/models.py
@@ -13,6 +13,17 @@ class Employee(BaseModel):
     birth_date = db.Column(db.Date, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(120), unique=True, nullable=False)
-    employments = relationship('Employment', foreign_keys='Employment.employee_id', back_populates='employee', cascade='all, delete-orphan')
     current_employment_id = db.Column(UUID, db.ForeignKey('employments.id'))
-    current_employment = relationship('Employment', foreign_keys=current_employment_id, cascade='all, delete-orphan', single_parent=True)
+    current_employment = relationship(
+        'Employment',
+        foreign_keys=current_employment_id,
+        cascade='all, delete-orphan',
+        single_parent=True)
+    employments = relationship(
+        'Employment',
+        foreign_keys='Employment.employee_id',
+        back_populates='employee',
+        cascade='all, delete-orphan')
+
+    def works_in_department(self, department_id):
+        return self.current_employment and self.current_employment.department_id == department_id
