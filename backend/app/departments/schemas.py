4f0dd5bbe2b840317089e5fd694217d291e6532a
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-26 13:16:33 +0000

    Removed unused post_dump hook and imports.

--- backend/app/departments/schemas.py
+++ backend/app/departments/schemas.py
@@ -1,12 +1,10 @@
 from flask import request
 from marshmallow import ValidationError
 from marshmallow import fields, validate
-from marshmallow import post_load
 from marshmallow import validates
 
 from app.employees.models import Employee
-from app.errors.errors import InvalidAttribute
-from app.errors.exceptions import NotFound, BadRequest
+from app.errors.exceptions import NotFound
 from app.schemas import ModelSchema
 
 
@@ -27,9 +25,3 @@ class DepartmentSchema(ModelSchema):
         if not employee.works_in_department(request.view_args.get('id')):
             raise ValidationError('Employee does not work in this department.')
 
-    @post_load
-    def uuid_to_hex(self, item):
-        uuid = item.get('head_id')
-        if uuid:
-            item['head_id'] = uuid.hex
-        return item
