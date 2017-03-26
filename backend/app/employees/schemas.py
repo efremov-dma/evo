acce6c011590716aa9b17eda2d6a758bdc2ebe64
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-26 13:17:22 +0000

    Added current employment id to the employee schema.

--- backend/app/employees/schemas.py
+++ backend/app/employees/schemas.py
@@ -1,5 +1,9 @@
+from marshmallow import ValidationError
 from marshmallow import fields, validate
+from marshmallow import validates
 
+from app.employments.models import Employment
+from app.errors.exceptions import NotFound
 from app.schemas import ModelSchema
 
 
@@ -9,3 +13,12 @@ class EmployeeSchema(ModelSchema):
     birth_date = fields.Date(required=True)
     email = fields.Email(required=True)
     phone = fields.Str(required=True)
+    current_employment_id = fields.UUID(allow_none=True)
+
+    @validates('current_employment_id')
+    def validate_employment_id(self, employment_id):
+        try:
+            Employment.get_or_404(employment_id.hex)
+        except NotFound:
+            raise ValidationError('Employments does not exist.')
+
