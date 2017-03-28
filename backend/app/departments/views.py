a63cd4e175ed1ef8d79c2050141dc9ab4b22ff00
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-28 20:54:12 +0000

    Order departments and positions by name.

--- backend/app/departments/views.py
+++ backend/app/departments/views.py
@@ -1,3 +1,4 @@
+from app import response
 from app.departments.models import Department
 from app.departments.schemas import DepartmentSchema
 from app.views import ListCreateView, ReadUpdateDeleteView
@@ -6,6 +7,11 @@ from app.views import ListCreateView, ReadUpdateDeleteView
 class DepartmentList(ListCreateView):
     model = Department
     schema = DepartmentSchema
+
+    def get(self):
+        data = self.model.query.order_by(self.model.name).all()
+
+        return response.success(data=data, schema=self.schema, many=True)
 
 
 class DepartmentSingle(ReadUpdateDeleteView):
--- backend/app/positions/views.py
+++ backend/app/positions/views.py
@@ -1,3 +1,4 @@
+from app import response
 from app.positions.models import Position
 from app.positions.schemas import PositionSchema
 from app.views import ListCreateView, ReadUpdateDeleteView
@@ -6,6 +7,11 @@ from app.views import ListCreateView, ReadUpdateDeleteView
 class PositionList(ListCreateView):
     model = Position
     schema = PositionSchema
+
+    def get(self):
+        data = self.model.query.order_by(self.model.name).all()
+
+        return response.success(data=data, schema=self.schema, many=True)
 
 
 class PositionSingle(ReadUpdateDeleteView):
