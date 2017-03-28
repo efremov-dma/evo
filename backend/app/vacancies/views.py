bd290b35eaa498dfc5a3fd3a8c519a07263c2b82
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-28 07:32:10 +0000

    Filter vacancies by department id.

--- backend/app/vacancies/views.py
+++ backend/app/vacancies/views.py
@@ -1,3 +1,6 @@
+from flask import request
+
+from app import response
 from app.vacancies.models import Vacancy
 from app.vacancies.schemas import VacancySchema
 from app.views import ListCreateView, ReadUpdateDeleteView
@@ -6,6 +9,14 @@ from app.views import ListCreateView, ReadUpdateDeleteView
 class VacancyList(ListCreateView):
     model = Vacancy
     schema = VacancySchema
+
+    def get(self):
+        if 'department_id' in request.args:
+            data = self.model.filter_by(department_id=request.args['department_id'])
+        else:
+            data = self.model.query.all()
+
+        return response.success(data=data, schema=self.schema, many=True)
 
 
 class VacancySingle(ReadUpdateDeleteView):
