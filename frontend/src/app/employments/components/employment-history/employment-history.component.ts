3a98b6e18a9bb1c8ce8aaca7fe02e08a84d056d9
Dmytro Yefremov <efremov.dma@gmail.com>
2017-03-28 20:47:43 +0000

    Fixed import paths in employment history component.

--- frontend/src/app/employments/components/employment-history/employment-history.component.ts
+++ frontend/src/app/employments/components/employment-history/employment-history.component.ts
@@ -1,10 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {OnInit} from '@angular/core';
-import {Employment} from '../models/employment';
-import {Employee} from '../../employees/models/employee';
-import {EmploymentService} from '../services/employment.service';
-import {MessageService} from '../../common/services/message.service';
-import {ResponseError} from '../../common/models/errors';
+import {Employment} from '../../models/employment';
+import {Employee} from '../../../employees/models/employee';
+import {EmploymentService} from '../../services/employment.service';
+import {MessageService} from '../../../common/services/message.service';
+import {ResponseError} from '../../../common/models/errors';
 
 
 @Component({
